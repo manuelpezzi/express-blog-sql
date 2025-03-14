c60bd281bc960240ab79a208b1bd6517d5acf8d9
manuelpezzi <pezzimanuel@gmail.com>
2025-03-14 09:57:45 +0100

    piccole correzioni

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,11 @@ function index(req, res) {
     const sql = 'SELECT * FROM posts';
 
     connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (err) return res.status(500).json({
+            error: 'Database query failed'
+
+        });
+
         res.json(results);
     });
 
@@ -36,9 +40,15 @@ function show(req, res) {
 
     connection.query(sql, [id], (err, results) => {
 
-        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (err) return res.status(500).json({
+            error: 'Database query failed'
 
-        if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
+        });
+
+        if (results.length === 0) return res.status(404).json({
+            error: 'Post  not found'
+
+        });
         res.json(results[0]);
     });
 }
